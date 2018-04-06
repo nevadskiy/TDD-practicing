c7186249f77547303c4f9841ad40c81b65c5961c
Nevadskiy <nevadskiy@gmail.com>
2018-04-07 00:05:09 +0300

    Setting Up test case

--- tests/unit/UserTest.php
+++ tests/unit/UserTest.php
@@ -5,62 +5,63 @@ use PHPUnit\Framework\TestCase;
 class UserTest extends TestCase
 {
 
+    protected $user;
+
+    public function setUp()
+    {
+        $this->user = new \App\Models\User;
+    }
+
     public function test_we_can_get_the_first_name()
     {
-        $user = new \App\Models\User;
 
-        $user->setFirstName('Vitalii');
+        $this->user->setFirstName('Vitalii');
 
-        $this->assertSame($user->getFirstName(), 'Vitalii');
+        $this->assertSame($this->user->getFirstName(), 'Vitalii');
     }
 
     public function test_we_can_get_the_last_name()
     {
-        $user = new \App\Models\User;
 
-        $user->setLastName('Nevadskiy');
+        $this->user->setLastName('Nevadskiy');
 
-        $this->assertSame($user->getLastName(), 'Nevadskiy');
+        $this->assertSame($this->user->getLastName(), 'Nevadskiy');
     }
 
     public function test_full_name_is_returned()
     {
-        $user = new \App\Models\User;
 
-        $user->setFirstName('Vitalii');
-        $user->setLastName('Nevadskiy');
+        $this->user->setFirstName('Vitalii');
+        $this->user->setLastName('Nevadskiy');
 
-        $this->assertSame($user->getFullName(), 'Vitalii Nevadskiy');
+        $this->assertSame($this->user->getFullName(), 'Vitalii Nevadskiy');
     }
 
     public function test_first_and_last_name_are_trimmed()
     {
-        $user = new \App\Models\User;
-        $user->setFirstName(' Vtalii      ');
-        $user->setLastName('  Nevadskiy ');
+        $this->user->setFirstName(' Vtalii      ');
+        $this->user->setLastName('  Nevadskiy ');
 
-        $this->assertSame($user->getFirstName(), 'Vtalii');
-        $this->assertSame($user->getLastName(), 'Nevadskiy');
+        $this->assertSame($this->user->getFirstName(), 'Vtalii');
+        $this->assertSame($this->user->getLastName(), 'Nevadskiy');
     }
 
     public function test_email_address_can_be_set()
     {
         $email = 'nevadskiy@gmail.com';
 
-        $user = new \App\Models\User;
-        $user->setEmail($email);
+        $this->user->setEmail($email);
 
-        $this->assertSame($user->getEmail(), $email);
+        $this->assertSame($this->user->getEmail(), $email);
     }
 
     public function test_email_variables_contains_correct_values()
     {
-        $user = new \App\Models\User;
-        $user->setFirstName('Vitalii');
-        $user->setLastName('Nevadskiy');
-        $user->setEmail('nevadskiy@gmail.com');
+        $this->user->setFirstName('Vitalii');
+        $this->user->setLastName('Nevadskiy');
+        $this->user->setEmail('nevadskiy@gmail.com');
 
-        $emailVariables = $user->getEmailVariables();
+        $emailVariables = $this->user->getEmailVariables();
 
         $this->assertArrayHasKey('full_name', $emailVariables);
         $this->assertArrayHasKey('email', $emailVariables);
